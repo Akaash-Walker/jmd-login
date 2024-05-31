fdf756a3bc5532690a82b93d861eca1a906f988f
Akaash-Walker <awalker7179@gmail.com>
2024-05-31 19:08:18 -0400

    Added routes and framework for todo list

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,4 +14,16 @@
             </div>
         </div>
     </div>
+   {{-- <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <form method="post" action="{{ route('saveItem') }}" accept-charset="UTF-8">
+                    {{ csrf_field() }}
+                    <label for="listItem">New Todo</label> <br>
+                    <input type="text" name="listItem"><br>
+                    <button type="submit">Save Item</button>
+                </form>
+            </div>
+        </div>
+    </div>--}}
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TodoListController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +18,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::post('/saveItem', [TodoListController::class, 'saveItem'])->name('saveItem');
 
 require __DIR__.'/auth.php';
